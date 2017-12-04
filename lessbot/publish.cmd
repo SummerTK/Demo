b0e8811039b84dbcf60fca3a7d5576692663a0a2
SummerTK <tang14455@163.com>
2017-12-04 14:51:36 +0800

    Revert "BOT_LUIS"
    This reverts commit 8fbe1b832570d81fa8c70818299235d58390fcdd.

--- lessbot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=lessbot-Web-Deploy.pubxml -p:Password=RJqp8SHBBP7zQk5cjfjamboEtfdoNjaXfbqvbx95mqNs2n3F9hy58ldggJHk
-
