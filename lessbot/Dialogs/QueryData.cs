8fbe1b832570d81fa8c70818299235d58390fcdd
SummerTK <tang14455@163.com>
2017-12-04 09:25:44 +0800

    BOT_LUIS

---
+++ lessbot/Dialogs/QueryData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuisBot.Dialogs
+{
+    public class QueryData
+    {
+
+    }
+
+    public class Data
+    {
+        public int ID { get; set; }
+        public string Question { get; set; }
+        public string Entity { get; set; }
+        public string Answer { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ lessbot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=lessbot-Web-Deploy.pubxml -p:Password=RJqp8SHBBP7zQk5cjfjamboEtfdoNjaXfbqvbx95mqNs2n3F9hy58ldggJHk
+
